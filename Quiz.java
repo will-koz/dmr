6937dc08e532cc5b4c42ba1cfd0662e66bce7986
Will <slorkda@gmail.com>
2023-04-27 20:14:57 -0700

    got num_questions working

--- Quiz.java
+++ Quiz.java
@@ -8,11 +8,11 @@ public class Quiz {
 private ArrayList<Question> questions;
 
 public Quiz () {
-	;
+	questions = new ArrayList<Question>();
 }
 
 public int num_correct_questions () { return 0; } // TEMP
-// public int num_questions () { return questions.size(); }
-public int num_questions () { return 0; } // TEMP
+public int num_questions () { return questions.size(); }
+// public int num_questions () { return 0; } // TEMP
 
 }
